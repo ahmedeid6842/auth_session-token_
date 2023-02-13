9e979060199a1a07ffa8e4c728f40fef7924ea3a
ahmed eid <ahmedeid6842@gmail.com>
2023-02-13 13:46:22 +0200

    removing unnecessary packages

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser");
-
-
 
 const tokenAuthRouter = require("./routes/usersToken");
 const sessionAuthRouter = require("./routes/usersSession");
 
 const app = express();
 
-
 app.use(express.json());
-
-
 app.use(cookieParser());
 
 require("./config/sessionConfig")(app);
@@ -23,11 +17,12 @@ app.use("/token", tokenAuthRouter);
 app.use("/session", sessionAuthRouter);
 
 const port = process.env.PORT || 4000;
-mongoose.connect(process.env.MONGODB_URI,
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
+mongoose
+    .connect(process.env.MONGODB_URI,
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
     .then(() => app.listen(port, console.log(`listeing on port ${port}`)))
-    // .catch(err => console.log(err));
+    .catch(err => console.log(err));
 
